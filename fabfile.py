78e091de8e2dc6b777fbe72d8ccfb54c27a3f873
MINETTE Alexandre <minette.alexandre@gmail.com>
2014-03-30 23:18:51 +0100

    improving the fabric script

--- fabfile.py
+++ fabfile.py
@@ -7,28 +7,62 @@ from fabric.api import *
 import time
 import datetime
 from fabric.colors import green, blue, red
-
+import json
 
 def deploy(app, git):
 	ts = int(time.time())
+
 	curdir = '/var/www/%s/versions/%s' % (app, ts)
 	run('mkdir -p %s' % (curdir))
 
 	run('git clone %s %s' % (git, curdir))
-	run('cp %s/app/config/parameters_prod.yml %s/app/config/parameters.yml' % (curdir, curdir))
-	run('cd %s && composer install --optimize-autoloader' % (curdir))
+	run('echo -n FAILED > /var/www/%s/versions/%s/.deploy-status' % (app, ts))
+	#run('cp %s/app/config/parameters_prod.yml %s/app/config/parameters.yml' % (curdir, curdir))
 
-	run('cd %s && app/console cache:clear' % (curdir))
-	run('cd %s && app/console cache:clear --env=prod' % (curdir))
-	run('cd %s && app/console cache:warmup' % (curdir))
-	run('cd %s && app/console cache:warmup --env=prod' % (curdir))
-	run('cd %s && app/console assetic:dump' % (curdir))
-	run('cd %s && app/console doctrine:database:create || true' % (curdir))
-	run('cd %s && app/console doctrine:schema:update --force' % (curdir))
-	run('cd %s && app/console doctrine:fixtures:load --append' % (curdir))
 
+	nodeploy = run('test -f /var/www/%s/versions/%s/deploy.sh || echo "nodeploy"' % (app, ts))
+	if nodeploy != 'nodeploy':
+		print blue('Executing the deploy.sh on the repository')
+		run('chmod +x /var/www/%s/versions/%s/deploy.sh' % (app, ts))
+		run('cd /var/www/%s/versions/%s && ./deploy.sh' % (app,ts))
+	else:
+		print red("No deploy.sh found in the repository, assuming that the application is deployed properly")
 	run('ln -s /var/www/%s/versions/%s /var/www/%s/_current' % (app,ts, app))
 	run('mv -T /var/www/%s/_current /var/www/%s/current' % (app, app))
+
+	run('echo -n SUCCESS > /var/www/%s/versions/%s/.deploy-status' % (app, ts))
+	print green('Application %s deployed properly' % (app))
+
+def getStatusList (app, versions):
+		statusList = []
+		for version in versions:
+			buildStatus = run('cat /var/www/%s/versions/%s/.deploy-status 2> /dev/null || true' % (app, int(version)))
+			if buildStatus == '':
+				buildStatus = 'UNKNOWN';
+			buildStatus = buildStatus.replace("\n","").replace("\r", '').replace(' ', '')
+			statusList.append(buildStatus)	
+		return statusList;
+
+def clean(app):
+	versions = run('ls /var/www/%s/versions/ | awk "{print $1}"' % (app))
+
+	current = run('perl -le \'print readlink(\"/var/www/%s/current\")\'' % (app))
+	curversion = int(current.split('/')[-1]);
+	
+	versionList = versions.split('\n');
+
+	statusList = getStatusList(app, versionList)
+	
+	deletedVersion = 0
+	i = 0
+	for version in versionList:
+		v = int(version)
+		if curversion != v:
+			if statusList[i] == 'UNKNOWN' or statusList[i] == 'FAILED':
+				run('rm -rf /var/www/%s/versions/%s' % (app, v))
+				deletedVersion = deletedVersion + 1
+		i = i + 1
+	print green('%s version deleted' % deletedVersion)
 
 
 def ls(app):
@@ -36,14 +70,22 @@ def ls(app):
 		current = run('perl -le \'print readlink(\"/var/www/%s/current\")\'' % (app))
 		curversion = int(current.split('/')[-1]);
 
+
+		i = 0
 		versionList = versions.split('\n');
+		statusList = getStatusList(app, versionList)
+
+		print "\n\n";
 		for version in versionList:
+			
 			dateAssociated = datetime.datetime.fromtimestamp(int(version)).strftime('%d-%m-%Y %H:%M:%S')
 			v = int(version)
+			buildString = ' > %s ( %s )  BuildStatus: %s' % (green(v), dateAssociated,  statusList[i])
 			if curversion == v:
-				print ' > ', green(v), ' (', dateAssociated, ')', red("[current]")
+				print buildString, red("[current]")
 			else:
-				print ' > ', green(v), ' (', dateAssociated, ')'
+				print buildString
+			i = i + 1
 
 def rollback(app, version):
 	versions = run('ls /var/www/%s/versions/%s' % (app, version))
